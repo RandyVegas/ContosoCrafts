a365088e221d2b74a7efb14aff26061f807cea24
i.tsarkov <i.tsarkov@tinkoff.ru>
2022-07-11 17:29:53 +0300

    init

---
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.Website.Models
+{
+	public class Product
+    {
+        public sting Id { get; set; }
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Img { get; set; }
+
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
